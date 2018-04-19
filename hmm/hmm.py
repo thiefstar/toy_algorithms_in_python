b807a67dcde2d5806c4214d576b7315b97e1a454
helm <ichaf@outlook.com>
2018-04-19 14:47:47 +0800

    update hmm seg

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -10,22 +10,62 @@
 import numpy as np
 import collections
 import pickle
+import re
+import math
 
+MIN_FLOAT = -3.14e100
 
-def build_dataset():
+PROB_START_P = "prob_start.plk"
+PROB_TRANS_P = "prob_trans.plk"
+PROB_EMIT_P = "prob_emit.plk"
+
+def build_datafile(fn, with_tagging=False):
     """
     构建数据集, 
-    (词频低于<?>的考虑过滤,用<unk>代替,看看效果会好一些吗)
-    :param dictionary: word2idx
-    :param reverse_dictionary: idx2word
+    (词频低于<?>的考虑过滤,用<unk>代替,看看效果会好一些吗, 按道理不会好)
+    :content: word0/[BMES] word1/[BMES] ... wordn/[BMES] \n
     """
-    # <unk> - -1
-    # <none> - 0 
-    return
+    # <unk> - 0
+    output_fn = fn + "_bmes"
+    fw = open(output_fn, 'w')
+    with open(fn, 'r') as f:
+        for line in f:
+            new_line = ""
+            if with_tagging:
+                for item in re.findall(".+?/\w+", line):
+                    item = item.split('/')[0].split()
+                    length = len(item)
+                    if length > 1:
+                        new_line += item[0] + "/B "
+                        for c in item[1:-1]:
+                            new_line += c + "/M "
+                        new_line += item[-1] + "/E "
+                    elif length == 1:
+                        new_line += item + "/S "    
+            else:
+                for item in line.strip().split():
+                    length = len(item)
+                    if length > 1:
+                        new_line += item[0] + "/B "
+                        for c in item[1:-1]:
+                            new_line += c + "/M "
+                        new_line += item[-1] + "/E "
+                    elif length == 1:
+                        new_line += item + "/S "    
+            new_line += "\n"
+            fw.write(new_line)
+    fw.close()
+    print("done.")
+
+def generate_data(fn):
+    with open(fn, 'r') as f:
+        for line in f:
+            data = [tuple(item.split('/')) for item in line.strip().split()]
+            yield data
 
 class HMM(object):
     """
-    Order 1 Hidden Markov Model
+    Order 1 Hidden Markov Model  # todo: expand
  
     Attributes
     ----------
@@ -95,14 +135,62 @@ class HMM(object):
         return X
 
     # 监督学习方法(统计)
-    def stati_calculate(self, save=False):
+    def stati_calculate(self, fn, states=('B','M','E','S'), save=False):
         """
         利用极大似然估计法来估计HMM参数
-        input: text file, every line: word/[BEMS], split with a blank space.
+        这边直接使用字典了(方便起见, 实际上不太好, 为统一性的话,需要转成序号)
+        jiaba里好像是对 频数 取log了
+        input: text file, every line: (word, [BEMS]) x len
         return: None (with updating of A, B, pi)
         """
-        pass
+        newA = dict.fromkeys(states)
+        for s in states:
+            newA[s] = {}
+        newB = dict.fromkeys(states)
+        for s in states:
+            newB[s] = {}
+        newpi = {}
+        for s in states:
+            newpi[s] = 0
+        for data in generate_data(fn):
+            data_len = len(data)
+            if data_len == 0:
+                continue
+            w, tag = data[0]
+            newpi[tag] = newpi.get(tag, 0) + 1
+            for i in range(1, data_len):
+                w_, tag_ = w, tag # i-1
+                w, tag = data[i]
 
+                newA[tag_][tag] = newA[tag_].get(tag, 0) + 1
+                newB[tag][w] = newB[tag].get(w, 0) + 1
+        
+        start_sum = sum(newpi.values())
+        for s_ in states:
+            a_sum = sum(newA[s_].values())
+            b_sum = sum(newB[s_].values())
+            for s in newA[s_].keys():
+                newA[s_][s] = math.log(newA[s_][s] / a_sum)
+            for w in newB[s_].keys():
+                newB[s_][w] = math.log(newB[s_][w] / b_sum)
+            if newpi[s_] != 0:
+                newpi[s_] = math.log(newpi[s_] / start_sum)
+            else:
+                newpi[s_] = MIN_FLOAT
+        self.A, self.B, self.pi = newA, newB, newpi
+        if save:
+            start_p = open(PROB_START_P, 'wb')
+            pickle.dump(newpi, start_p)
+            start_p.close()
+            trans_p = open(PROB_TRANS_P, 'wb')
+            pickle.dump(newA, trans_p)
+            trans_p.close()
+            emit_p = open(PROB_EMIT_P, 'wb')
+            pickle.dump(newB, emit_p)
+            emit_p.close()
+            print("saved.")
+
+            
     # 非监督学习方法
     def baum_welch_train(self, observations, criterion=0.05):
         """
@@ -152,36 +240,57 @@ class HMM(object):
 
 if __name__ == '__main__':
 
-    from viterbi_hmm import viterbi
+    # from viterbi_hmm import viterbi
 
-    h = HMM(np.array([[ 0.7,0.3],
-                    [0.4,0.6]]),
-            np.array([[ 0.5,0.4,0.1],
-                    [ 0.1,0.3,0.6]]),
-            np.array([0.6, 0.4]))
+    # h = HMM(np.array([[ 0.7,0.3],
+    #                 [0.4,0.6]]),
+    #         np.array([[ 0.5,0.4,0.1],
+    #                 [ 0.1,0.3,0.6]]),
+    #         np.array([0.6, 0.4]))
 
-    observations_data, states_data = h.simulate(100)
-    print(observations_data)
-    print(states_data)
+    # observations_data, states_data = h.simulate(100)
+    # print(observations_data)
+    # print(states_data)
 
-    guess = HMM(np.array([[0.5, 0.5],
-                          [0.5, 0.5]]),
-                np.array([[0.3, 0.3, 0.3],
-                          [0.3, 0.3, 0.3]]),
-                np.array([0.5, 0.5])
-    )
+    # guess = HMM(np.array([[0.5, 0.5],
+    #                       [0.5, 0.5]]),
+    #             np.array([[0.3, 0.3, 0.3],
+    #                       [0.3, 0.3, 0.3]]),
+    #             np.array([0.5, 0.5])
+    # )
 
-    guess.baum_welch_train(observations_data)
-    print()
-    print(h.pi)
-    print(h.A)
-    print(h.B)
-    prob, path = viterbi(observations_data, [0, 1], h.pi, h.A, h.B)
-    p = 0.0
-    print(path)
-    for i in range(len(path)):
-        if path[i] == states_data[i]:
-            p += 1
-    print(p / len(states_data))
-        
+    # guess.baum_welch_train(observations_data)
+    # print()
+    # print(h.pi)
+    # print(h.A)
+    # print(h.B)
+    # prob, path = viterbi(observations_data, [0, 1], h.pi, h.A, h.B)
+    # p = 0.0
+    # print(path)
+    # for i in range(len(path)):
+    #     if path[i] == states_data[i]:
+    #         p += 1
+    # print(p / len(states_data))
     
+    # 仅使用msr_training数据集(Bakeoff 2005)
+    # build_datafile(fn='msr_training.utf8')
+
+    from word_seg import viterbi, load_params
+
+    # h = HMM(0, 0, 0)
+    # h.stati_calculate(fn='msr_training.utf8_bmes', save=True)
+    start_p, trans_p, emit_p = load_params()
+    obs = "演出结束后，江泽民等党和国家领导人走上舞台，亲切会见了参加演出的全体人员，祝贺演出成功，并与他们合影留念。"
+    prob, states = viterbi(obs, ('B','M','E','S'), start_p, trans_p, emit_p)
+    print(states)
+    seg = ""
+    for i, tag in enumerate(states):
+        if tag == 'S':
+            seg += obs[i]
+            seg += " "
+        elif tag == 'B' or tag == 'M':
+            seg += obs[i]
+        elif tag == 'E':
+            seg += obs[i]
+            seg += " "
+    print(seg)
--- hmm/word_seg.py
+++ hmm/word_seg.py
@@ -11,7 +11,9 @@ PROB_START_P = "prob_start.plk"
 PROB_TRANS_P = "prob_trans.plk"
 PROB_EMIT_P = "prob_emit.plk"
 
-# 稍晚简化了一些
+# todo: 要用的话,1 封装; 2 需大一点的语料训练
+
+# 对viterbi, 稍晚简化了一些, 考虑到了obs不存在的情况(当然只是简单的添加极小值)
 PrevStatus = {
     'B': 'ES',
     'M': 'MB',
@@ -20,12 +22,22 @@ PrevStatus = {
 }
 
 def load_params():
-    pass
+    start_f = open(PROB_START_P, 'rb')
+    start_p = pickle.load(start_f)
+    start_f.close()
+    trans_f = open(PROB_TRANS_P, 'rb')
+    trans_p = pickle.load(trans_f)
+    trans_f.close()
+    emit_f = open(PROB_EMIT_P, 'rb')
+    emit_p = pickle.load(emit_f)
+    emit_f.close()
+    return start_p, trans_p, emit_p
 
 def viterbi(obs, states, start_p, trans_p, emit_p):
     V = [{}]  # tabular
     path = {}
     for y in states:  # init
+        # 如果不存在obs in emit_p, 返回min_float, 表示可能性很低
         V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
         path[y] = [y]
     for t in range(1, len(obs)):
@@ -43,8 +55,47 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
 
     return (prob, path[state])
 
-def _cat(sentence):
+def _cut(sentence):
     pass
 
-def cat(sentence):
+def cut(sentence):
     pass
+
+if __name__ == '__main__':
+
+    # 对 Bakeoff 2005 测试集做测试
+    test_fn = "icwb2-data/testing/msr_test.utf8"
+    output_fn = "icwb2-data/testing/msr_test_seg.utf8"
+    start_p, trans_p, emit_p = load_params()
+    fw = open(output_fn, 'w')
+    with open(test_fn, 'r') as f:
+        for line in f:
+            obs = line.strip()
+            prob, states = viterbi(obs, ('B','M','E','S'), start_p, trans_p, emit_p)
+            seg = ""
+            for i, tag in enumerate(states):
+                if tag == 'S':
+                    seg += obs[i]
+                    seg += " "
+                elif tag == 'B' or tag == 'M':
+                    seg += obs[i]
+                elif tag == 'E':
+                    seg += obs[i]
+                    seg += " "
+            fw.write(seg+"\n")
+    fw.close()
+    """ 使用msr语料训练的结果(基于hmm, viteribi)
+    === SUMMARY:
+    === TOTAL INSERTIONS:   7859
+    === TOTAL DELETIONS:    5091
+    === TOTAL SUBSTITUTIONS:        16278
+    === TOTAL NCHANGE:      29228
+    === TOTAL TRUE WORD COUNT:      106873
+    === TOTAL TEST WORD COUNT:      109641
+    === TOTAL TRUE WORDS RECALL:    0.800
+    === TOTAL TEST WORDS PRECISION: 0.780
+    === F MEASURE:  0.790
+    === OOV Rate:   0.026
+    === OOV Recall Rate:    0.363
+    === IV Recall Rate:     0.812
+    """
